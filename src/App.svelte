fa7e7d583e840cca892a21309920166c088b3c76
unsignd <unsigndid@gmail.com>
2024-07-26 05:16:15 -0700

    code header logo action

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,10 @@
 
 <main>
   <header class="header">
-    <p class="title">./connected</p>
+    <div class="logo">
+      <a class="github" href="https://github.com/unsignd/connected" target="_blank">github repo by any chance?</a>
+      <p class="title">./connected</p>
+    </div>
     <div class="group">
       <button on:click={() => window.open(document.URL, '_blank')}>add new point</button>
       <button on:click={() => localStorage.removeItem('data')}>clear data</button>
@@ -15,7 +18,7 @@
 
 <style>
   main {
-    padding: 0 16px;
+    padding: 16px;
   }
 
   .header {
@@ -24,8 +27,44 @@
     justify-content: space-between;
   }
 
-  .header > .title {
+  .header > .logo {
+    width: 250px;
+    height: 32px;
+
+    display: flex;
+    align-items: center;
+
+    position: relative;
+
+    overflow: hidden;
+  }
+
+  .header > .logo > .title {
+    position: absolute;
+    
     font-weight: 600;
+
+    transition: margin 150ms ease-in-out;
+  }
+
+  .header > .logo > .github {
+    position: absolute;
+    margin-top: -64px;
+
+    font-weight: 600;
+    text-decoration: underline;
+    text-decoration-thickness: 2px;
+
+    transition: margin 150ms ease-in-out;
+    cursor: pointer;
+  }
+
+  .header > .logo:hover > .title {
+    margin-top: 64px;
+  }
+
+  .header > .logo:hover > .github {
+    margin-top: 0;
   }
 
   .header > .group {
